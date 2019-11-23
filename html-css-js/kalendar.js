757a40e8a287e8bbfbafc6df55abb37ea83daa5f
kkadusic <kkadusic1@etf.unsa.ba>
2019-11-23 23:32:38 +0100

    Spirala 2

---
+++ html-css-js/kalendar.js
@@ -0,0 +1,327 @@
+let Kalendar = (function(){
+
+	let danas = new Date();
+	let mjesec = danas.getMonth();
+	let godina = danas.getFullYear();
+	let sviMjeseci = ["Januar", "Februar", "Mart", "April", "Maj", "Juni", "Juli", "August", "Septembar", "Oktobar", "Novembar", "Decembar"];
+
+	var redovnaZauzeca;
+	var vanrednaZauzeca;
+
+	function dajMjesecImpl(){
+		return mjesec;
+	}
+
+
+	function ocistiPodatkeImpl(){
+		redovnaZauzeca = [];
+		vanrednaZauzeca = [];
+	}
+
+	function ucitajPodatkeImpl(periodicna, vanredna){
+		redovnaZauzeca = periodicna;
+		vanrednaZauzeca = vanredna;
+	}
+
+
+	function dajIndeksTrazenogDana(mjesec, dan){
+		if (mjesec == 0){
+			if (dan == 0) return 6;
+			if (dan == 1) return 0;
+			if (dan == 2) return 1;
+			if (dan == 3) return 2;
+			if (dan == 4) return 3;
+		} 
+		if (mjesec == 1){
+			if (dan == 0) return 3;
+			if (dan == 1) return 4;
+			if (dan == 2) return 5;
+			if (dan == 3) return 6;
+			if (dan == 4) return 0;
+		} 
+		if (mjesec == 2){
+			if (dan == 0) return 3;
+			if (dan == 1) return 4;
+			if (dan == 2) return 5;
+			if (dan == 3) return 6;
+			if (dan == 4) return 0;
+		} 
+		if (mjesec == 3){
+			if (dan == 0) return 0;
+			if (dan == 1) return 1;
+			if (dan == 2) return 2;
+			if (dan == 3) return 3;
+			if (dan == 4) return 4;
+		} 
+		if (mjesec == 4){
+			if (dan == 0) return 5;
+			if (dan == 1) return 6;
+			if (dan == 2) return 0;
+			if (dan == 3) return 1;
+			if (dan == 4) return 2;
+		} 
+		if (mjesec == 5){
+			if (dan == 0) return 2;
+			if (dan == 1) return 3;
+			if (dan == 2) return 4;
+			if (dan == 3) return 5;
+			if (dan == 4) return 6;
+		} 
+		if (mjesec == 6){
+			if (dan == 0) return 0;
+			if (dan == 1) return 1;
+			if (dan == 2) return 2;
+			if (dan == 3) return 3;
+			if (dan == 4) return 4;
+		} 
+		if (mjesec == 7){
+			if (dan == 0) return 4;
+			if (dan == 1) return 5;
+			if (dan == 2) return 6;
+			if (dan == 3) return 0;
+			if (dan == 4) return 1;
+		} 
+		if (mjesec == 8){
+			if (dan == 0) return 1;
+			if (dan == 1) return 2;
+			if (dan == 2) return 3;
+			if (dan == 3) return 4;
+			if (dan == 4) return 5;
+		} 
+		if (mjesec == 9){
+			if (dan == 0) return 6;
+			if (dan == 1) return 0;
+			if (dan == 2) return 1;
+			if (dan == 3) return 2;
+			if (dan == 4) return 3;
+		} 
+		if (mjesec == 10){
+			if (dan == 0) return 3;
+			if (dan == 1) return 4;
+			if (dan == 2) return 5;
+			if (dan == 3) return 6;
+			if (dan == 4) return 0;
+		} 
+		if (mjesec == 11){
+			if (dan == 0) return 1;
+			if (dan == 1) return 2;
+			if (dan == 2) return 3;
+			if (dan == 3) return 4;
+			if (dan == 4) return 5;
+		}
+	}
+
+
+
+	function obojiRedovnoZauzece(mjesec, dan){
+		let brojDanaMjeseca = 32 - new Date(godina, mjesec, 32).getDate();
+		let prviDanMjeseca = ((new Date(godina, mjesec)).getDay() + 6) % 7;
+		var duzina = document.getElementsByClassName("kalendarBroj");
+
+		let a = dajIndeksTrazenogDana(mjesec, dan);
+
+		for (var i=prviDanMjeseca + a; i < duzina.length; i+=7){
+			var x = document.getElementsByClassName("kalendarBroj")[i];
+			var y = x.children[1].className = "zauzeta";
+		}
+	}
+
+
+
+	function obojiVanrednoZauzece(odabraniMjesec, danDatum){
+		let prviDanMjeseca = ((new Date(godina, odabraniMjesec)).getDay() + 6) % 7;
+		var x = document.getElementsByClassName("kalendarBroj")[danDatum + prviDanMjeseca - 1];
+		var y = x.children[1].className = "zauzeta";
+	}
+
+
+
+
+	function obojiZauzecaImpl(kalendarRef, mjesec, sala, pocetak, kraj){
+        let semestar = "";
+
+        if (kalendarRef != null && sala != null && pocetak != null && kraj != null && 
+        	sala != "" && pocetak != "" && kraj != ""){
+        	
+        	for (var i=0; i<redovnaZauzeca.length; i++){
+        		if (redovnaZauzeca[i].naziv == sala){
+        			semestar = redovnaZauzeca[i].semestar;
+        			if (((semestar == "zimski") && (mjesec == 0 || mjesec >= 9)) ||
+        				((semestar == "ljetni") && (mjesec >= 1 && mjesec <= 5))){
+        				if((pocetak <= redovnaZauzeca[i].kraj) && 
+        					(redovnaZauzeca[i].pocetak <= kraj)){
+        					obojiRedovnoZauzece(mjesec, redovnaZauzeca[i].dan);
+        				
+        				}
+        			}
+        		}
+        	}
+        	
+        	
+        	for (var i=0; i<vanrednaZauzeca.length; i++){
+        		if (vanrednaZauzeca[i].naziv == sala){
+        			if((pocetak <= vanrednaZauzeca[i].kraj) && 
+        					(vanrednaZauzeca[i].pocetak <= kraj)){
+						var dan = Number(vanrednaZauzeca[i].datum.substring(0,2));
+						var odabraniMjesec = Number(vanrednaZauzeca[i].datum.substring(3,5));
+						if (mjesec == odabraniMjesec-1){
+							obojiVanrednoZauzece(odabraniMjesec-1, dan);
+						}
+        				
+        			}
+        		}
+        	}
+        	
+        }
+	}
+
+	
+
+
+
+
+	function obrisi(elementID){
+    	document.getElementById(elementID).innerHTML = "";
+	}
+
+
+
+	function prethodniImpl(){
+		if (godina === 2019 && mjesec > 0){
+			obrisi('kalendarDatum');
+			godina = (mjesec === 0) ? godina - 1 : godina;
+    		mjesec = (mjesec === 0) ? 11 : mjesec - 1;
+			iscrtajKalendarImpl(document.getElementById("kalendarDatum"), mjesec);
+			if (document.getElementById("sala").value != null && 
+				document.getElementById("pocetak").value != null && 
+				document.getElementById("kraj").value){
+				Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), Kalendar.dajMjesec(), 
+					document.getElementById("sala").value, document.getElementById("pocetak").value, document.getElementById("kraj").value);
+			}
+		}
+		else alert("Prikaz prethodne godine je onemogucen");
+	}
+
+
+
+	function sljedeciImpl(){
+		if (godina === 2019 && mjesec < 11){
+			obrisi('kalendarDatum');
+			godina = (mjesec === 11) ? godina + 1 : godina;
+    		mjesec = (mjesec + 1) % 12;
+			iscrtajKalendarImpl(document.getElementById("kalendarDatum"), mjesec);
+			if (document.getElementById("sala").value != null && 
+				document.getElementById("pocetak").value != null && 
+				document.getElementById("kraj").value){
+				Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), Kalendar.dajMjesec(), 
+					document.getElementById("sala").value, document.getElementById("pocetak").value, document.getElementById("kraj").value);
+			}
+		}
+		else alert("Prikaz sljedece godine je onemogucen");
+	}
+
+
+
+
+	function iscrtajKalendarImpl(kalendarRef, mjesec){
+		let prviDanMjeseca = ((new Date(godina, mjesec)).getDay() + 6) % 7;
+		let brojDanaMjeseca = 32 - new Date(godina, mjesec, 32).getDate();
+
+		// Postavljanje naziva za mjesec
+		obrisi('kalendarMjesec');
+		var el = document.createElement('div');
+		var domString = '<h3>' + sviMjeseci[mjesec] + '</h3>';
+		el.innerHTML = domString;
+		document.getElementById("kalendarMjesec").appendChild(el.firstChild);
+	
+		// Dodavanje dana iz proslog mjeseca
+		for (var i = 0; i < prviDanMjeseca; i++){
+			var el = document.createElement('div');
+			var domString = '<div class="kalendarBroj danBezPrikaza"></div>';
+			el.innerHTML = domString;
+			kalendarRef.appendChild(el.firstChild);
+		}
+	
+		// Dodavanje dana iz aktuelnog mjeseca
+		for (var i = 1; i <= brojDanaMjeseca; i++){
+			var el = document.createElement('div');
+			var domString = '<div class="kalendarBroj"> <div class="danBroj">' + i + '</div> <div class="slobodna"></div> </div>';
+			el.innerHTML = domString;
+			kalendarRef.appendChild(el.firstChild);
+		} 
+	}
+
+
+	
+	return {
+		obojiZauzeca: obojiZauzecaImpl,
+		ucitajPodatke: ucitajPodatkeImpl,
+		iscrtajKalendar: iscrtajKalendarImpl,
+		prethodni: prethodniImpl,
+		sljedeci: sljedeciImpl,
+		dajMjesec: dajMjesecImpl,
+		ocistiPodatke: ocistiPodatkeImpl
+	}
+}());
+
+
+
+
+
+// Podaci za ucitavanje
+var redovna = [{
+		dan: 3,
+		semestar: "zimski",
+		pocetak: "12:00",
+		kraj: "15:00",
+		naziv: "A3",
+		predavac: "John"
+	}, 
+	{
+		dan: 1,
+		semestar: "zimski",
+		pocetak: "12:00",
+		kraj: "15:00",
+		naziv: "A3",
+		predavac: "John"
+	}];
+
+var vanredna = [{
+		datum: "15.11.2019",
+		pocetak: "12:00",
+		kraj: "15:00",
+		naziv: "A2",
+		predavac: "Taylor"
+	}, 
+	{
+		datum: "27.11.2019",
+		pocetak: "15:00",
+		kraj: "18:00",
+		naziv: "1-08",
+		predavac: "Ben"
+	}];
+
+window.onload = function() {
+	let trenutniMjesec = new Date().getMonth();
+	Kalendar.ucitajPodatke(redovna, vanredna);
+    Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), trenutniMjesec);
+};
+
+document.getElementById("forma").addEventListener("change", myFunction);
+
+function myFunction() {
+	//alert("Promjena na formi");
+	//Kalendar.ucitajPodatke(redovna, vanredna);
+	//Kalendar.obrisi("kalendarDatum");
+	//Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 3);
+	//Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), trenutniMjesec);
+ 	var odabranaSala = document.getElementById("sala").value;
+	var pocetak = document.getElementById("pocetak").value;
+	var kraj = document.getElementById("kraj").value;
+  	Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), Kalendar.dajMjesec(), odabranaSala, pocetak, kraj);
+  	Kalendar.obrisi("kalendarDatum");
+}
+
+
+//primjer korištenja modula
+//Kalendar.obojiZauzeca(document.getElementById("kalendar"),1,"A3","12:00","13:30");
---
+++ html-css-js/testRezervacija.js
@@ -0,0 +1,521 @@
+let assert = chai.assert;
+describe('Kalendar', function () {
+  describe('iscrtajKalendar()', function () {
+
+    it('Pozivanje iscrtajKalendar za mjesec sa 30 dana:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 10); //Novembar
+      let polja = document.getElementsByClassName("slobodna");
+      assert.equal(polja.length, 30, "Očekivano je da se prikaže 30 dana");
+    });
+
+    it('Pozivanje iscrtajKalendar za mjesec sa 31 dan:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 9); //Oktobar
+      let polja = document.getElementsByClassName("slobodna");
+      assert.equal(polja.length, 31, "Očekivano je da se prikaže 31 dan");
+    });
+
+    it('Pozivanje iscrtajKalendar za trenutni mjesec:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 10); //Novembar
+      let dan = document.getElementsByClassName("kalendarBroj")[4].textContent;
+      assert.equal(" 1  ", dan, "Očekivano je da je 1. dan u petak");
+    });
+
+    it('Pozivanje iscrtajKalendar za trenutni mjesec:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 10); //Novembar
+      let dan = document.getElementsByClassName("kalendarBroj")[33].textContent;
+      assert.equal(" 30  ", dan, "Očekivano je da je 30. dan u subotu");
+    });
+
+    it('Pozivanje iscrtajKalendar za januar:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 0); //Januar
+      let brojDana = document.getElementsByClassName("slobodna");
+      let prviDan = document.getElementsByClassName("kalendarBroj")[1].textContent;
+      let zadnjiDan = document.getElementsByClassName("kalendarBroj")[31].textContent;
+      assert.equal(brojDana.length, 31, "Očekivano je da se prikaže 31 dan");
+      assert.equal(" 1  ", prviDan, "Očekivano je da je 1. dan u utorak");
+      assert.equal(" 31  ", zadnjiDan, "Očekivano je da je 30. dan u subotu");
+    });
+
+    it('Pozivanje iscrtajKalendar za juni:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 5); //Juni
+      let brojDana = document.getElementsByClassName("slobodna");
+      let prviDan = document.getElementsByClassName("kalendarBroj")[5].textContent;
+      let zadnjiDan = document.getElementsByClassName("kalendarBroj")[34].textContent;
+      assert.equal(brojDana.length, 30, "Očekivano je da se prikaže 30 dana");
+      assert.equal(" 1  ", prviDan, "Očekivano je da je 1. dan u subotu");
+      assert.equal(" 30  ", zadnjiDan, "Očekivano je da je 30. dan u nedjelju");
+    });
+
+    it('Pozivanje iscrtajKalendar za septembar:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 8); //Septembar
+      let brojDana = document.getElementsByClassName("slobodna");
+      let prviDan = document.getElementsByClassName("kalendarBroj")[6].textContent;
+      let zadnjiDan = document.getElementsByClassName("kalendarBroj")[35].textContent;
+      assert.equal(brojDana.length, 30, "Očekivano je da se prikaže 30 dana");
+      assert.equal(" 1  ", prviDan, "Očekivano je da je 1. dan u nedjelju");
+      assert.equal(" 30  ", zadnjiDan, "Očekivano je da je 30. dan u ponedjeljak");
+    });
+
+  });
+
+
+  describe('iscrtajKalendar(), ucitajPodatke()', function () {
+
+    it('Pozivanje obojiZauzeca kada podaci nisu učitani:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+      let mjesec = new Date().getMonth();
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), mjesec);
+      let brojDanaMjeseca = 32 - new Date(2019, mjesec, 32).getDate();
+      Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), Kalendar.dajMjesec(), "", "", "");
+      let polja = document.getElementsByClassName("slobodna");
+      assert.equal(polja.length, brojDanaMjeseca, "Očekivana vrijednost da se ne oboji niti jedan dan");
+    });
+
+
+    it('Pozivanje obojiZauzeca gdje u zauzećima postoje duple vrijednosti za zauzeće istog termina:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+
+      var redovna = [{
+          dan: 1,
+          semestar: "ljetni",
+          pocetak: "11:00",
+          kraj: "15:00",
+          naziv: "A3",
+          predavac: "John"
+        },
+        {
+          dan: 3,
+          semestar: "zimski",
+          pocetak: "11:00",
+          kraj: "15:00",
+          naziv: "A1",
+          predavac: "Anna"
+        }
+      ];
+
+      var vanredna = [{
+          datum: "27.11.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "27.11.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        }
+      ];
+      Kalendar.ucitajPodatke(redovna, vanredna);
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 10);
+      Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), Kalendar.dajMjesec(), "A2", "12:00", "15:00");
+      var x = document.getElementsByClassName("kalendarBroj")[30];
+      var y = x.children[1].className;
+      assert.equal(y, "zauzeta", "Očekivano je da se dan oboji bez obzira što postoje duple vrijednosti");
+
+    });
+
+
+    it('Pozivanje obojiZauzece kada u podacima postoji periodično zauzeće za drugi semestar:', function () {
+      var redovna = [{
+        dan: 2,
+        semestar: "ljetni",
+        pocetak: "11:00",
+        kraj: "15:00",
+        naziv: "A2",
+        predavac: "John"
+      }];
+
+      var vanredna = [{
+        datum: "25.8.2019",
+        pocetak: "09:00",
+        kraj: "10:00",
+        naziv: "0-05",
+        predavac: "John"
+      }];
+
+      Kalendar.ucitajPodatke(redovna, vanredna);
+      document.getElementById("kalendarDatum").innerHTML = "";
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 10);
+      Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), Kalendar.dajMjesec(), "A2", "12:00", "15:00");
+
+      let polja = document.getElementsByClassName("slobodna");
+      assert.equal(polja.length, 30, "Očekivano je da se ne oboji zauzeće");
+
+    });
+
+    it('Dva puta uzastopno pozivanje obojiZauzece:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+
+      var redovna = [{
+        dan: 1,
+        semestar: "ljetni",
+        pocetak: "11:00",
+        kraj: "15:00",
+        naziv: "A3",
+        predavac: "John"
+      }];
+
+      var vanredna = [{
+          datum: "27.11.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "27.11.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        }
+      ];
+      Kalendar.ucitajPodatke(redovna, vanredna);
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 10);
+      Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), Kalendar.dajMjesec(), "A2", "12:00", "15:00");
+      Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), Kalendar.dajMjesec(), "A2", "12:00", "15:00");
+      var x = document.getElementsByClassName("kalendarBroj")[30];
+      var y = x.children[1].className;
+      assert.equal(y, "zauzeta", "Očekivano je da boja zauzeća ostane ista");
+    });
+
+
+    it('Pozivanje obojiZauzece kada su u podacima svi termini u mjesecu zauzeti:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+
+      var redovna = [{
+          dan: 1,
+          semestar: "ljetni",
+          pocetak: "11:00",
+          kraj: "15:00",
+          naziv: "A3",
+          predavac: "John"
+        },
+        {
+          dan: 3,
+          semestar: "zimski",
+          pocetak: "11:00",
+          kraj: "15:00",
+          naziv: "A1",
+          predavac: "Anna"
+        }
+      ];
+
+      var vanredna = [{
+          datum: "01.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "02.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "03.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "04.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "05.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "06.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "07.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "08.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "09.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "10.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "11.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "12.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "13.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "14.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "15.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "16.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "17.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "18.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "19.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "20.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "21.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "22.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "23.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "24.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "25.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "26.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "27.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "28.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "29.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        },
+        {
+          datum: "30.04.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        }
+      ];
+      Kalendar.ucitajPodatke(redovna, vanredna);
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 3);
+      Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), 3, "A2", "12:00", "15:00");
+
+      let polja = document.getElementsByClassName("slobodna");
+      assert.equal(polja.length, 0, "Očekivano je da se svi dani oboje");
+
+    });
+
+
+    it('Pozivanje ucitajPodatke, obojiZauzeca, ucitajPodatke - drugi podaci, obojiZauzeca:', function () {
+      document.getElementById("kalendarDatum").innerHTML = "";
+
+      var redovna = [{
+        dan: 1,
+        semestar: "ljetni",
+        pocetak: "11:00",
+        kraj: "15:00",
+        naziv: "A3",
+        predavac: "John"
+      }];
+
+      var vanredna = [{
+          datum: "10.06.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A2",
+          predavac: "John"
+        }
+      ];
+      Kalendar.ucitajPodatke(redovna, vanredna);
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 5);
+      Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), 5, "A2", "11:00", "15:00");
+
+      document.getElementById("kalendarDatum").innerHTML = "";
+      var redovna = [{
+        dan: 1,
+        semestar: "ljetni",
+        pocetak: "11:00",
+        kraj: "15:00",
+        naziv: "A3",
+        predavac: "John"
+      }];
+
+      var vanredna = [{
+          datum: "12.03.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "0-06",
+          predavac: "John"
+        }
+      ];
+      Kalendar.ucitajPodatke(redovna, vanredna);
+      Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 2);
+      Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), 2, "0-06", "11:00", "15:00");
+
+      var x = document.getElementsByClassName("kalendarBroj")[15];
+      var y = x.children[1].className;
+      assert.equal(y, "zauzeta", "Očekivano da se zauzeća iz prvih podataka ne ostanu obojena, tj. primjenjuju se samo posljednje učitani podaci");
+      
+
+    });
+
+
+
+    it('Pozivanje obojiZauzece kada u podacima postoji zauzeće termina ali u drugom mjesecu:', function() {
+        var redovna = [{
+          dan: 2,
+          semestar: "ljetni",
+          pocetak: "11:00",
+          kraj: "15:00",
+          naziv: "0-03",
+          predavac: "John"
+        }];
+
+        var vanredna = [{
+          datum: "15.09.2019",
+          pocetak: "12:00",
+          kraj: "15:00",
+          naziv: "A1",
+          predavac: "Anna"
+        }];
+
+        document.getElementById("kalendarDatum").innerHTML = "";
+        Kalendar.iscrtajKalendar(document.getElementById("kalendarDatum"), 9);
+        Kalendar.ucitajPodatke(redovna, vanredna);
+        Kalendar.obojiZauzeca(document.getElementById("kalendarMjesec"), 9, "A1", "12:00", "15:00");
+        
+        let polja = document.getElementsByClassName("slobodna");
+        assert.equal(polja.length, 31, "Očekivano je da se ne oboji zauzeće");
+      });
+
+
+
+  });
+
+});
